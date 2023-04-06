2e97aa8f5510db0974a222686888fe2075a66581
colecrum26 <cwcrumrine@gmail.com>
2023-04-06 15:01:30 -0500

    completed exercises

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-console.log("Hello World!\n==========\n");
-
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
@@ -7,32 +5,42 @@ const http = require("http");
 
 // Finish setting up the server
 
-http.createServer((req, res) => {
+http
+  .createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     res.setHeader("content-type", "text/html");
     console.log(`request method used: ${method}`);
     const dataChunksArr = [];
-    req.on('data', (chunk) =>{
-        console.log(`chunk is: ${chunk}`)
-        dataChunksArr.push(chunk);
+    req.on("data", (chunk) => {
+      console.log(`chunk is: ${chunk}`);
+      dataChunksArr.push(chunk);
     });
-    req.on('end', () => {
-        if (method == "POST") {
-            const body = JSON.parse(Buffer.concat(dataChunksArr).toString());
-            const resBody = { method, url, body};
-        } 
+    req.on("end", () => {
+      const body = JSON.parse(Buffer.concat(dataChunksArr).toString());
+      const resBody = { method, url, body };
+      if (method == "POST") {
         if (url == "/") {
-            res.write("<h1>WELCOME CITIZEN")
-        } else if (url == "/about") {
-            res.write("<p>I am not sure what I'm doing, so bear with.")
+          res.statusCode = 200;
+          res.write("<h1>WELCOME CITIZEN</h1>");
+          console.log("home route");
         } else if (url == "/echo") {
-            res.write(resBody);
-            console.log(resBody)
+          res.statusCode = 200;
+          res.write(JSON.stringify(resBody));
+        } else {
+          res.statusCode = 404;
+          res.write("PAGE NOT FOUND");
         }
         res.end();
-    })
-    .listen(3000, () => {
-        console.log("Server is listening on port 3000.");
-    })
-});
+      }
+    });
+
+    if (url == "/about") {
+      res.statusCode = 200;
+      res.write("<p>I am not sure what I'm doing, so bear with.</p>");
+      res.end();
+    }
+  })
+  .listen(5001, () => {
+    console.log("Server is listening on port 3000.");
+  });
